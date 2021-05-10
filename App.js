7d1ca39db616bfe448f37af481be6baf0640b757
Ishrat Eliza <37586198+elizaan@users.noreply.github.com>
2021-05-10 16:05:18 +0600

    noob try

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+  // let x = 1;
+  const handlePress = (what) =>{
+    console.log( what + " pressed");
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress("Text")}>Hello!!</Text>
+
+      <TouchableOpacity>
+        <Image onPress={handlePress("Image")}
+        blurRadius={1}
+        fadeDuration={500}
+        source ={require('./assets/favicon.png')}></Image>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        {/* <Button 
+        color="blue"
+        title = "Add" onPress={() => Alert.alert("Hello", "Button Pressed",[
+          {text: "Yes", onPress: () => Alert.alert("Congratulation!")},
+          {text: "No", onPress: () => Alert.alert("Ops, bad luck!")}])}
+        /> */}
+         <Button 
+        color="blue"
+        title = "Add" onPress={() => 
+          Alert.alert("Done", "Button successfull",[
+            {text: "Yes", onPress: () => Alert.alert("Congratulation!")},
+            {text: "No", onPress: () => Alert.alert("Ops, bad luck!")}])}
+        />
+      </TouchableOpacity>
+
+      
+     
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +45,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: Platform.OS === "Android" ? StatusBar.CurrentHeight : 0
+    // alignItems: 'center',
+    // justifyContent: 'center',
+
   },
 });
