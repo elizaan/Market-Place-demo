19a580ba67dcb038148c195fc119164fe90a26cd
Ishrat Eliza <37586198+elizaan@users.noreply.github.com>
2021-05-11 14:29:23 +0600

    dimension tutorial

--- App.js
+++ App.js
@@ -1,42 +1,22 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Platform, StatusBar } from 'react-native';
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 
 export default function App() {
+  const {landscape} = useDeviceOrientation();
   // let x = 1;
-  const handlePress = (what) =>{
-    console.log( what + " pressed");
-  }
+  // const handlePress = (what) =>{
+  //   console.log( what + " pressed");
+  // }
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress("Text")}>Hello!!</Text>
-
-      <TouchableOpacity>
-        <Image onPress={handlePress("Image")}
-        blurRadius={1}
-        fadeDuration={500}
-        source ={require('./assets/favicon.png')}></Image>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
-        {/* <Button 
-        color="blue"
-        title = "Add" onPress={() => Alert.alert("Hello", "Button Pressed",[
-          {text: "Yes", onPress: () => Alert.alert("Congratulation!")},
-          {text: "No", onPress: () => Alert.alert("Ops, bad luck!")}])}
-        /> */}
-         <Button 
-        color="blue"
-        title = "Add" onPress={() => 
-          Alert.alert("Done", "Button successfull",[
-            {text: "Yes", onPress: () => Alert.alert("Congratulation!")},
-            {text: "No", onPress: () => Alert.alert("Ops, bad luck!")}])}
-        />
-      </TouchableOpacity>
-
-      
-     
-      {/* <StatusBar style="auto" /> */}
+      <View 
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: landscape ? "100%" : "30%"
+      }}></View>
     </SafeAreaView>
   );
 }
