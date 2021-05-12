4ab4675e46dbe199bbeacb9a52e3620d4e0f3244
Ishrat Eliza <37586198+elizaan@users.noreply.github.com>
2021-05-12 06:03:17 +0600

    welcome & image screen done

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Platform, StatusBar } from 'react-native';
-import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
+import {StyleSheet, Platform, StatusBar} from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen.js';
+import WelcomeScreen from './app/screens/WelcomeScreen.js';
+
+// import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
+
 
 export default function App() {
-  const {landscape} = useDeviceOrientation();
-  // let x = 1;
-  // const handlePress = (what) =>{
-  //   console.log( what + " pressed");
-  // }
-  return (
-    <SafeAreaView style={styles.container}>
-      <View 
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%"
-      }}></View>
-    </SafeAreaView>
-  );
+  
+  // return <WelcomeScreen/>;
+  return <ViewImageScreen/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingVertical: Platform.OS === "Android" ? StatusBar.CurrentHeight : 0
-    // alignItems: 'center',
-    // justifyContent: 'center',
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     paddingVertical: Platform.OS === "Android" ? StatusBar.CurrentHeight : 0
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
 
-  },
-});
+//   },
+// });
